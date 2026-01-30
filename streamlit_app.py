4dc738922383c9819097fc3f997f285417cbbd13
rshaw13 <ryanshaw959@gmail.com>
2026-01-30 17:39:45 +1100

    changes to formatting. adding wind map layer

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,12 +22,23 @@ m = folium.Map(location=[-30, 145], zoom_start=4.5, tiles="CartoDB positron")
 
 scale = 0.15
 
+openweathermap_api_key = st.secrets["OPENWEATHERMAP_API_KEY"]
+
 for _, row in df.iterrows():
 
-    popup_text = (
-        f"{row['Station Name']}<br>"
-        f"{row['SCADAVALUE']} MW / {row['REG_CAP']} MW<br>"
-        f"{row['utilisation_pct']}%"
+    wind_tiles = (
+        f"https://tile.openweathermap.org/map/wind_new/{z}/{x}/{y}.png?appid={openweathermap_api_key}"
+    )
+
+    folium.raster_layers.TileLayer(
+        tiles=wind_tiles,
+        attr="OpenWeatherMap",
+        name="Wind",
+        overlay=True,
+        control=True,
+    ).add_to(m)
+
+    popup_text = (f"{row['Station Name']}\n{row['SCADAVALUE']} MW Output / {row['REG_CAP']} MW Capacity.\n({row['utilisation_%']:.1f}% Utilisation)"
     )
 
     folium.CircleMarker(
@@ -38,6 +49,7 @@ for _, row in df.iterrows():
         fill_color="green" if row["utilisation_pct"] > 50 else "red",
         stroke=False,
         popup=popup_text, 
+        tooltip=popup_text,
     ).add_to(m)
 
     folium.CircleMarker(
@@ -45,7 +57,10 @@ for _, row in df.iterrows():
         radius=row["REG_CAP"] * scale,
         color="gray",
         weight=1,
-        fill=False,
+        fill_opacity=0,
+        fill_color="black",
+        tooltip=popup_text,
+        popup=popup_text,
     ).add_to(m)
 
 st_folium(m, width=1400, height=700)
